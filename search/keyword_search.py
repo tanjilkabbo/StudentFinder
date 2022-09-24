33feca817d961582bff0a551b3a10c0e6b4d913d
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-09-24 19:52:28 +0600

    Add: keyword_search.py "This Class will improve the search quality later"

---
+++ search/keyword_search.py
@@ -0,0 +1,104 @@
+import re
+import time
+
+import winsound
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+
+
+class SearchKeyword:
+    def pageStart(self, driver):
+        time.sleep(2)
+        driver.implicitly_wait(30)
+        pageStartAction = ActionChains(driver)
+        pageStartAction.send_keys(Keys.HOME)
+        pageStartAction.perform()
+
+    def scrollDown(self, driver):
+        time.sleep(2)
+        driver.implicitly_wait(30)
+        scrollDownAction = ActionChains(driver)
+        scrollDownAction.send_keys(Keys.PAGE_DOWN)
+        scrollDownAction.perform()
+
+        # print(input("Press any Key: "))
+
+    def scrollUp(self, driver):
+        time.sleep(2)
+        driver.implicitly_wait(30)
+        scrollUpAction = ActionChains(driver)
+        scrollUpAction.send_keys(Keys.PAGE_UP)
+        scrollUpAction.perform()
+
+    def scrollAndSearchUsingXpath(self, driver):
+        for search in range(2):
+            time.sleep(5)
+            driver.implicitly_wait(30)
+            self.scrollDown(driver)
+        for search in range(2):
+            time.sleep(5)
+            driver.implicitly_wait(30)
+            self.scrollUp(driver)
+        grpupPostXpath = "//div[@aria-label='Actions for this post']"
+        grpupPostXpathAria = driver.find_elements(By.XPATH, grpupPostXpath)
+        # print(grpupPostXpathAria)
+        print("First " + str(len(grpupPostXpathAria)) + " Post are searching by bot")
+
+        index = []
+        for postLoad in grpupPostXpathAria:
+            time.sleep(4)
+            driver.implicitly_wait(30)
+            index.append(postLoad)
+            grpupPostXpathAria[len(index)-1].click()
+            grpupPostXpathAria[len(index)-1].click()
+        self.pageStart(driver)
+
+
+    def searchWordUsingXpath(self, driver):
+        search_interested_word_xpath = "//div[contains(text(),interested)]"
+        searchWordhAria = driver.find_elements(By.XPATH, search_interested_word_xpath)
+        # print(searchWordhAria)
+        # print(str(len(searchWordhAria)) + " interested Word Found")
+
+        if driver.find_element(By.XPATH, search_interested_word_xpath):
+            print(str(searchWordhAria[0]) + " :is the Elements" + "\n" + search_interested_word_xpath + " :For this xpath")
+            # print(searchWordhAria[0].text)
+            allText = searchWordhAria[0].text
+
+            text = allText
+
+            index = []
+            for m in re.finditer(r"\binterested\b", text):
+                if m.group(0):
+                    index.append(m)
+                    print("Present")
+
+                    freq = 500
+                    # duration is set to 100 milliseconds
+                    dur = 100
+                    winsound.Beep(freq, dur)
+                else:
+                    print("Absent")
+                print(str(len(index)) + " no 'interested' Word Found")
+
+            if len(index) != 0:
+                print("Total " + str(len(index)) + " 'interested' Word Found")
+                print(input("Press any Key: "))
+            else:
+                print("No 'interested' Word Found")
+
+
+# from driver.driver import Driver
+# driver = Driver().driver
+#
+# driver.get("https://www.facebook.com")
+# from login.login import Login
+# Login().login(driver)
+# driver.get("https://www.facebook.com/groups/308915099815447")
+#
+# SearchKeyword().scrollAndSearchUsingXpath(driver)
+# SearchKeyword().searchWordUsingXpath(driver)
+
+
+
+
--- search/search_important_post.py
+++ search/search_important_post.py
@@ -3,6 +3,7 @@ import time
 from driver.driver import Driver
 from googlesheet.connection import Connection
 from login.login import Login
+from search.keyword_search import SearchKeyword
 
 driver = Driver().driver
 driver.get("https://facebook.com")
@@ -15,15 +16,19 @@ group_list = work_sheet.col_values(1)
 print(group_list)
 
 
-def visit_link_list(link_list):
+def visit_link_list(driver, link_list):
     for link in link_list:
         print(link)
         driver.get(link)
         time.sleep(2)
         driver.implicitly_wait(4)
+
+        SearchKeyword().scrollAndSearchUsingXpath(driver)
+        SearchKeyword().searchWordUsingXpath(driver)
+
     print(f"\nWe visit {len(link_list)} link")
     return len(link_list)
 
 
-visit_link_list(group_list)
+visit_link_list(driver, group_list)
 
