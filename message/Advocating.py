146cb919c179664bbdfa627f96f5a245f415fd9f
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-10-10 07:54:33 +0600

    Update : Can comment base on index

--- message/Advocating.py
+++ message/Advocating.py
@@ -1,7 +1,7 @@
 """
 We test our advocacy in this page : https://www.facebook.com/groups/341334433330410/permalink/1266678044129373/
 """
-from selenium.webdriver import ActionChains
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 from message.message_text import MessageText
 from datetime import datetime
@@ -9,13 +9,15 @@ import time
 from driver.driver import Driver
 from googlesheet.connection import Connection
 from login.login import Login
+import re
 
 
-message = MessageText()
+message = "Are you using broadband internet ?"
 # print(first_message)
 
 
 driver = Driver().driver
+action = ActionChains(driver)
 driver.get("https://facebook.com")
 
 
@@ -38,20 +40,57 @@ previous_comments = driver.find_element(By.XPATH, "//span[contains(text(),'previ
 print(previous_comments)
 previous_comments.click()
 
-time.sleep(10)
+time.sleep(5)
 driver.implicitly_wait(10)
-all_comments_fb_profile = driver.find_elements(By.XPATH, "//span[@class='x3nfvp2']")
-print(len(all_comments_fb_profile))
+main_comments = driver.find_elements(By.XPATH, "//div[@role='article']")
+print(len(main_comments))
 
-action = ActionChains(driver)
+main_comments_reply_button = driver.find_elements(By.XPATH, "//div[@role='button'][normalize-space()='Reply']")
+print(len(main_comments_reply_button))
 
-for fb_profile in all_comments_fb_profile:
-    action.move_to_element(fb_profile).perform()
-    time.sleep(4)
-    print(fb_profile.text)
-    if fb_profile.text == 'Chimala Poisonivy Hicks':
-        action.context_click(fb_profile)
-        print(input("Stop :"))
+comment_index = []
+for comment in main_comments:
+    comment_index.append(comment)
+    print(f"Index : {len(comment_index)}")
+    print(comment.text)
+    re.finditer(r"\binterested\b", comment.text)
+    # TODO : Base on regular expression create condition
+    action.move_to_element(comment).click(comment).perform()
+    main_comments_reply_button[len(comment_index)-1].click()
+    action.send_keys(message).perform()
+    # time.sleep(5)
+    print("......................")
+    print(input("Stop :"))
+
+
+# time.sleep(10)
+# driver.implicitly_wait(10)
+# all_comments_fb_profile = driver.find_elements(By.XPATH, "//span[@class='x3nfvp2']")
+# print(len(all_comments_fb_profile))
+#
+# action = ActionChains(driver)
+#
+# for fb_profile in all_comments_fb_profile:
+#     action.move_to_element(fb_profile).perform()
+#     time.sleep(4)
+#     print(fb_profile.text)
+#     if fb_profile.text == 'Garrowolf Sauber':
+#         # action.move_to_element(fb_profile)
+#         action.move_to_element(fb_profile).\
+#             key_down(Keys.CONTROL).\
+#             click(fb_profile).\
+#             key_up(Keys.CONTROL).\
+#             send_keys(Keys.TAB).\
+#             send_keys(Keys.TAB).\
+#             send_keys(Keys.TAB).\
+#             send_keys(Keys.TAB).\
+#             send_keys(Keys.TAB). \
+#             pause(2). \
+#             perform()
+#         time.sleep(2)
+#         # action.click().perform()
+#
+#         print(input("Stop :"))
 
 
 print(input("Stop :"))
